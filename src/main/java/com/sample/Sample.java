1b1b7806dce17c12f4182bce0035917aab7f7e43
Adinarayana Bikkina <adinarayana@gmail.com>
2021-05-22 15:02:00 +0530

    initial version

---
+++ src/main/java/com/sample/Sample.java
@@ -0,0 +1,22 @@
+package com.sample;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	
+@Test
+	public  void sample1() {
+		System.out.println("Hello adinarayana");
+	}
+
+@Test	
+	public  void sample2() {
+		System.out.println("How are you????");
+	}
+	
+@Test
+	public  void sample3() {
+		System.out.println("what is your plans today");
+	}
+
+}
